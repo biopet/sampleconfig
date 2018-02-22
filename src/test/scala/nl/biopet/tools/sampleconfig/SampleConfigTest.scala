220c7647dda31df6589f8183ca0b1a193b1b813f
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-02-22 11:42:59 +0100

    add tests

--- src/test/scala/nl/biopet/tools/sampleconfig/SampleConfigTest.scala
+++ src/test/scala/nl/biopet/tools/sampleconfig/SampleConfigTest.scala
@@ -21,6 +21,8 @@
 
 package nl.biopet.tools.sampleconfig
 
+import java.io.{ByteArrayOutputStream, File}
+
 import nl.biopet.utils.test.tools.ToolTest
 import org.testng.annotations.Test
 
@@ -31,5 +33,157 @@ class SampleConfigTest extends ToolTest[Args] {
     intercept[IllegalArgumentException] {
       SampleConfig.main(Array())
     }
+  }
+
+  @Test
+  def testSamples(): Unit = {
+    val baos = new ByteArrayOutputStream
+    Console.withOut(baos) {
+      SampleConfig.main(Array("-i", resourcePath("/samples.yml")))
+    }
+    val samples = new String(baos.toByteArray).split("\n").toList.sorted
+    samples shouldBe List("sample1", "sample2")
+  }
+
+  @Test
+  def testLibraries(): Unit = {
+    val baos = new ByteArrayOutputStream
+    Console.withOut(baos) {
+      SampleConfig.main(
+        Array("-i", resourcePath("/samples.yml"), "--sample", "sample1"))
+    }
+    val libraries = new String(baos.toByteArray).split("\n").toList.sorted
+    libraries shouldBe List("lib1")
+  }
+
+  @Test
+  def testReadgroups(): Unit = {
+    val baos = new ByteArrayOutputStream
+    Console.withOut(baos) {
+      SampleConfig.main(
+        Array("-i",
+              resourcePath("/samples.yml"),
+              "--sample",
+              "sample1",
+              "--library",
+              "lib1"))
+    }
+    val readgroups = new String(baos.toByteArray).split("\n").toList.sorted
+    readgroups shouldBe List("rg1")
+  }
+
+  @Test
+  def testSampleConfig(): Unit = {
+    val jsonOutput = File.createTempFile("test.", ".json")
+    jsonOutput.deleteOnExit()
+    val tsvOutput = File.createTempFile("test.", ".tsv")
+    tsvOutput.deleteOnExit()
+
+    SampleConfig.main(
+      Array("-i",
+            resourcePath("/samples.yml"),
+            "--sample",
+            "sample1",
+            "--jsonOutput",
+            jsonOutput.getAbsolutePath,
+            "--tsvOutput",
+            tsvOutput.getAbsolutePath))
+  }
+
+  @Test
+  def testLibraryConfig(): Unit = {
+    val jsonOutput = File.createTempFile("test.", ".json")
+    jsonOutput.deleteOnExit()
+    val tsvOutput = File.createTempFile("test.", ".tsv")
+    tsvOutput.deleteOnExit()
+
+    SampleConfig.main(
+      Array(
+        "-i",
+        resourcePath("/samples.yml"),
+        "--sample",
+        "sample1",
+        "--library",
+        "lib1",
+        "--jsonOutput",
+        jsonOutput.getAbsolutePath,
+        "--tsvOutput",
+        tsvOutput.getAbsolutePath
+      ))
+  }
+
+  @Test
+  def testReadgroupConfig(): Unit = {
+    val jsonOutput = File.createTempFile("test.", ".json")
+    jsonOutput.deleteOnExit()
+    val tsvOutput = File.createTempFile("test.", ".tsv")
+    tsvOutput.deleteOnExit()
+
+    SampleConfig.main(
+      Array(
+        "-i",
+        resourcePath("/samples.yml"),
+        "--sample",
+        "sample1",
+        "--library",
+        "lib1",
+        "--readgroup",
+        "rg1",
+        "--jsonOutput",
+        jsonOutput.getAbsolutePath,
+        "--tsvOutput",
+        tsvOutput.getAbsolutePath
+      ))
+  }
+
+  @Test
+  def testNoTags(): Unit = {
+    intercept[IllegalArgumentException] {
+      SampleConfig.main(
+        Array("-i", resourcePath("/samples.yml"), "--jsonOutput", "."))
+    }.getMessage shouldBe "No tags found to write a file"
+
+    intercept[IllegalArgumentException] {
+      SampleConfig.main(
+        Array("-i", resourcePath("/samples.yml"), "--tsvOutput", "."))
+    }.getMessage shouldBe "No tags found to write a file"
+  }
+
+  @Test
+  def testWrongSample(): Unit = {
+    intercept[IllegalStateException] {
+      SampleConfig.main(
+        Array("-i", resourcePath("/samples.yml"), "--sample", "not_exist"))
+    }.getMessage shouldBe "Sample 'not_exist' not found"
+  }
+
+  @Test
+  def testWrongLibrary(): Unit = {
+    intercept[IllegalStateException] {
+      SampleConfig.main(
+        Array("-i",
+              resourcePath("/samples.yml"),
+              "--sample",
+              "sample1",
+              "--library",
+              "not_exist"))
+    }.getMessage shouldBe "Library 'not_exist' in sample 'sample1' not found"
+  }
+
+  @Test
+  def testWrongReadgroup(): Unit = {
+    intercept[IllegalStateException] {
+      SampleConfig.main(
+        Array("-i",
+              resourcePath("/samples.yml"),
+              "--sample",
+              "sample1",
+              "--library",
+              "lib1",
+              "--readgroup",
+              "not_exist",
+              "--tsvOutput",
+              "."))
+    }.getMessage shouldBe "Readgroup 'not_exist' in library 'lib1' in sample 'sample1' not found"
   }
 }
